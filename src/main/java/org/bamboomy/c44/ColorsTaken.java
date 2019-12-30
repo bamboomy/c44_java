0e572b9c5b12d03942e0b56a41ff0bf6581cc6da
Sander Theetaert <sander.theetaert@realdolmen.com>
2019-12-30 17:38:22 +0100

    blauwe kater

---
+++ src/main/java/org/bamboomy/c44/ColorsTaken.java
@@ -0,0 +1,25 @@
+package org.bamboomy.c44;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class ColorsTaken {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+private String game;
+	
+	private String color;
+	
+	private String name;
+	
+	private String javaHash;
+	}
---
+++ src/main/java/org/bamboomy/c44/ColorsTakenRepository.java
@@ -0,0 +1,11 @@
+package org.bamboomy.c44;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface ColorsTakenRepository extends CrudRepository<ColorsTaken, Integer> {
+
+}
