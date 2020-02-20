263f988cc3743e11bb769db310adfacfbead7c2b
Sander <bamboomy@gmail.com>
2020-02-20 20:47:48 +0100

    dev

--- src/main/java/org/bamboomy/c44/board/Board.java
+++ src/main/java/org/bamboomy/c44/board/Board.java
@@ -4,24 +4,18 @@ import java.util.ArrayList;
 import java.util.concurrent.locks.ReentrantLock;
 
 import org.bamboomy.c44.ColorsTaken;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@PropertySource({ "classpath:env.properties" })
 public class Board {
 
-	@Autowired
-	private Environment env;
-	
+	@Value("${path}")
 	@Getter
 	private String path;
-	
-	{
-		path = env.getProperty("path");
-	}
-	
 
 	@Getter
 	private Place[][] placez = new Place[12][12];
